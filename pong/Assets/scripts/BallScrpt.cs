ee595bcf3d2257c85127ea440a87f23222cb1384
BMeruva2001 <67717516+BMeruva2001@users.noreply.github.com>
2021-05-12 19:07:48 +0530

    final

---
+++ pong/Assets/scripts/BallScrpt.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallScrpt : MonoBehaviour
+{
+
+    public float ballSpeed;
+    public float inc;
+    Vector2 ballDirection;
+
+    Rigidbody2D rb;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ballDirection = Vector2.one.normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = ballDirection * ballSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Wall"))
+        {
+            ballDirection.y = -ballDirection.y;
+        }
+        else if (collision.gameObject.CompareTag("Block"))
+        {
+            ballDirection.x = -ballDirection.x;
+            ballSpeed += inc;
+        }
+    }
+
+    private void Update()
+    {
+        if(transform.position.x < -15.5 || transform.position.x > 15.5 || transform.position.y > 5.7 || transform.position.y < -5.7)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
---
+++ pong/Assets/scripts/BlockScrpt.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class BlockScrpt : MonoBehaviour
+{
+    public float blockSpeed;
+    public float minY;
+    public float maxY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.name == "rightBlock")
+        {
+            float moveY = Input.GetAxisRaw("rightBlock") * blockSpeed * Time.deltaTime;
+            transform.position = new Vector2(transform.position.x, math.clamp(transform.position.y + moveY,minY,maxY));
+        }
+        else if (gameObject.name == "leftBlock")
+        {
+            float moveY = Input.GetAxisRaw("leftBlock") * blockSpeed * Time.deltaTime;
+            transform.position = new Vector2(transform.position.x , math.clamp(transform.position.y + moveY, minY, maxY));
+        }
+    }
+}
